c7dbe358660089aa1c4ec4e49eed6f8a24dbc241
zhdaniukivan <iz2819815@gmail.com>
2024-07-23 20:22:15 +0300

    added time_checker decorator to func get_chat_answer

--- main/service/openai_service.py
+++ main/service/openai_service.py
@@ -1,6 +1,22 @@
+import datetime
+
 from g4f.client import Client
+import datetime
+question = 'Какая площадь Минска'
+model = "gpt-3.5-turbo"
+messages = [{"role": "user", "content": f'{question}, отвечай пожалуйста на русcком языке'}]
 
 
+def time_checker(func):
+    def wrapper(*args, **kwargs):
+        start_time = datetime.datetime.now()
+        result = func(*args, **kwargs)
+        print(datetime.datetime.now()-start_time)
+        return result
+    return wrapper
+
+
+@time_checker
 class OpenAIService:
     @staticmethod
     def get_chat_answer(self, model: str, messages: list) -> str:
@@ -10,3 +26,6 @@ class OpenAIService:
             return 'Повторите попытку сервер перегружен'
         print(response.choices[0].message.content)
         return response.choices[0].message.content
+
+
+# res = OpenAIService.get_chat_answer(model, messages)
